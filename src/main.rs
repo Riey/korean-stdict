9880b6e05fbdd2e2d92a4f13a9fbbd0a86a9c65b
Riey <creeper844@gmail.com>
2021-03-10 21:06:30 +0900

    Init commit

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+use quick_xml::{events::Event, Reader};
+use rayon::prelude::*;
+use std::collections::BTreeMap;
+use std::path::{Path, PathBuf};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Args {
+    files: Vec<PathBuf>,
+}
+
+fn process(file: &Path) -> Result<Vec<(String, String)>, quick_xml::Error> {
+    let mut out = Vec::with_capacity(1000);
+    let mut buf = Vec::with_capacity(1024);
+    let mut reader = Reader::from_file(file)?;
+    let mut word = String::new();
+    let mut hanja = String::new();
+
+    loop {
+        match reader.read_event(&mut buf)? {
+            Event::Start(ref e) => {
+                match unsafe { std::str::from_utf8_unchecked(e.name()) } {
+                    "word" => {
+                        // drop text
+                        reader.read_event(&mut buf)?;
+                        if let Event::CData(ref data) = reader.read_event(&mut buf)? {
+                            word = data.unescape_and_decode(&reader)?;
+                            // word = std::str::from_utf8(data.escaped())?
+                            //     .replace(|c| matches!(c, '-' | '^'), "");
+                        }
+                    }
+                    "original_language" => {
+                        // drop text
+                        reader.read_event(&mut buf)?;
+                        if let Event::CData(ref data) = reader.read_event(&mut buf)? {
+                            hanja = data.unescape_and_decode(&reader)?;
+                        }
+                    }
+                    "language_type" => {
+                        reader.read_event(&mut buf)?;
+                        if let Event::CData(ref data) = reader.read_event(&mut buf)? {
+                            if data.escaped() == "한자".as_bytes() {
+                                out.push((std::mem::take(&mut word), std::mem::take(&mut hanja)));
+                            }
+                        }
+                    }
+                    _ => {}
+                }
+            }
+            Event::Eof => break,
+            _ => {}
+        }
+    }
+
+    Ok(out)
+}
+
+fn main() {
+    let args = Args::from_args();
+
+    let dict: BTreeMap<String, String> = args
+        .files
+        .into_par_iter()
+        .flat_map(|file| process(&file).unwrap_or_default())
+        .collect();
+
+    println!("{:#?}", dict);
+}
